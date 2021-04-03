7043d2d19e991f7d503fedfe39b58a61386fa164
xinyuex0312 <65756616+xinyuex0312@users.noreply.github.com>
2021-04-03 19:42:59 +0900

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-vi test.txt +$'i\nhihi12333' +w +q
+
+vi abc.txt << EOF
+ i                        # 进入 insert 模式
+ Here is a document!      # 输入文本内容
+ ^[                       # 意为按下ESC退出编辑模式
+ :wq                      # 保存退出
+EOF
