857152a7287be4cccb557d740e403351528d9200
xinyuex0312 <65756616+xinyuex0312@users.noreply.github.com>
2021-04-03 19:08:53 +0900

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "import $1"
-int=1
-while(( $int<=5 ))
-do
-    echo " the number is $int "
-    let "int++"
-done
+#! /bin/bash
+vi abc.txt << EOF
+ i                        # 进入 insert 模式
+ Here is a document!      # 输入文本内容
+ ^[                       # 意为按下ESC退出编辑模式
+ :wq                      # 保存退出
+EOF
